0d9a5d44ebb7fb84a7a9bff508e5603b8c50b5a2
Lindsay Chapin <lnchapin@gmail.com>
2019-06-08 03:57:39 -0600

    security

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,13 @@ const Div = styled.div`
   margin-bottom: 1em;
   background: url(/images/bloom.jpg) no-repeat fixed center center / cover rgba(0, 0, 0, 0);
   height: 100vh;
-  overflow: hidden;
+`
+const Content = styled.div`
   color: #FFFFFF;
   padding-left: 10px;
   font-family: 'Roboto', sans-serif;
 `
+
 const Link = styled.a`
   color: white;
   :hover {
@@ -19,9 +21,16 @@ const Link = styled.a`
   }
 `
 const Name = styled.h1`
-  font-size: 150px;
+  font-size: 9.375rem;
   font-family: 'IBM Plex Sans', sans-serif;
-  filter: drop-shadow(0 0 0.75rem black)
+  filter: drop-shadow(0 0 0.75rem black);
+
+    @media only screen and (max-width: 768px) {
+      font-size: 4.2rem;
+    }
+    @media only screen and (max-width: 640px) {
+      font-size: 2.93rem;
+    }
 `
 
 
@@ -29,21 +38,23 @@ const Name = styled.h1`
 const Header = () => {
   return (
     <Div>
-      <Row>
-        <Col m={6} s={12}>
-        <Name>Lindsay Chapin</Name>
-        </Col>
-      </Row>
-      <Row>
-        <Col s={5}>
-        <h3>Full Stack Web Developer</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col s={4}>
-        <h3><Link href="https://drive.google.com/file/d/0B3dJEXOREOMeSGxnU3lCQ3hiNlk/view?usp=sharing" target="_blank" rel="noopener noreferrer">View My Resume</Link></h3>
-        </Col>
-      </Row>
+      <Content>
+        <Row>
+          <Col m={6} s={12}>
+          <Name>Lindsay Chapin</Name>
+          </Col>
+        </Row>
+        <Row>
+          <Col s={5}>
+          <h3>Full Stack Web Developer</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col s={4}>
+          <h3><Link href="https://drive.google.com/file/d/0B3dJEXOREOMeSGxnU3lCQ3hiNlk/view?usp=sharing" target="_blank" rel="noopener noreferrer">View My Resume</Link></h3>
+          </Col>
+        </Row>
+      </Content>
     </Div>
   )
 }
